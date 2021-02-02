f026584f9afdd7882f3c64e84b63c985d7051db6
brunotot <brunotot10000@gmail.com>
2021-02-02 18:06:03 +0100

    Add PROCITAJ_ME.txt

--- start.bat
+++ start.bat
@@ -9,7 +9,7 @@ start /min npm start
 cd angular-src
 start /min npm start
 cls
-echo Opening application in 15 seconds
+echo Opening application in 30 seconds
 echo We opened two terminals for you to view backend and frontend service logs, check them out
-TIMEOUT /T 15 /NOBREAK
+TIMEOUT /T 30 /NOBREAK
 start "" http://localhost:4200
